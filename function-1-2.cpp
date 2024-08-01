18e0923527f89d1275ae4406d64ae55be2b6e710
Jiyan2901 <you@example.com>
2024-08-01 09:53:39 +0930

    m2u

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-int is_identity(int array[3][3]){
+int is_identity(int array[10][10]){
     int flag = 1;
 
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
             if(i==j){
                 if(array[i][j] != 1){
                     flag = 0;
