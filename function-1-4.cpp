f93e2665bc3005b2722cdba5742f730032ff060b
Jiyan2901 <you@example.com>
2024-08-01 10:29:24 +0930

    m4u

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void print_scaled(int array[3][3],int scale){
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            array[i][j] *= 3;
+            array[i][j] *= scale;
             cout<<array[i][j]<<" ";
         }
         cout<<endl;
