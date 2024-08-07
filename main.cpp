e8f816b141a738bdc0e578e058f6e63381ddf461
Jiyan2901 <you@example.com>
2024-08-07 19:24:01 +0930

    main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "workshop.h"
 
 extern void changeValue(double*);
+extern void printArray(double*,int);
 
 int main(){
-    double x = 30;
-    
+    double x = 30;     
     changeValue(&x);
+
+    double array[5] = {1.1,2.2,3.3,4.4,5.5};
+    printArray(array,5);
 }
--- workshop.h
+++ workshop.h
@@ -4,3 +4,10 @@ void changeValue(double* x){
     *x = 42;
     std::cout<<*x<<std::endl;
 }
+
+void printArray(double* x,int y){
+    for(int i=0;i<5;i++){
+        std::cout<<x[i]<<" ";
+    }
+    std::cout<<std::endl;
+}
