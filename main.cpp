eaddb6ab77389374d77982c259f4310994249e4f
Jiyan2901 <you@example.com>
2024-08-07 19:33:44 +0930

    main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 extern void changeValue(double*);
 extern void printArray(double*,int);
+extern double arrayMax(double*,int);
 
 int main(){
     double x = 30;     
@@ -10,4 +11,7 @@ int main(){
 
     double array[5] = {1.1,2.2,3.3,4.4,5.5};
     printArray(array,5);
+
+    double maximum_element_array[5] = {23.6,131.3,95.9,-200.0,0.0};    
+    std::cout<<arrayMax(maximum_element_array,5)<<std::endl;
 }
--- workshop.h
+++ workshop.h
@@ -6,8 +6,20 @@ void changeValue(double* x){
 }
 
 void printArray(double* x,int y){
-    for(int i=0;i<5;i++){
+    for(int i=0;i<y;i++){
         std::cout<<x[i]<<" ";
     }
     std::cout<<std::endl;
+}
+
+double arrayMax(double* x,int y){
+    double max = x[0];
+
+    for(int i=0;i<y;i++){
+        if(x[i] > max){
+            max = x[i];
+        }
+    }
+    
+    return max;
 }
