544d8e53e4a79dd705fff37b25e8424165d94d14
Jiyan2901 <you@example.com>
2024-08-21 22:54:58 +0930

    main

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include "Orchestra.h"
 #include "Musician.h"
-#include "Musician.cpp"
+
+Musician::Musician(){
+    instrument_main = "null";
+    experience_main = 0;
+}
+
+Musician::Musician(std::string instrument, int experience){
+    instrument_main = instrument;
+    experience_main = experience;
+}
+
+std::string Musician::get_instrument(){
+    return instrument_main;
+}
+
+int Musician::get_experience(){
+    return experience_main;
+}
 
 int main() {
     // create an empty orchestra
