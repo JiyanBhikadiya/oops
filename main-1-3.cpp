ac26c360413fbd9430dd102f086b256ef0b1ad8a
Jiyan2901 <you@example.com>
2024-08-29 23:10:26 +0930

    main

---
+++ main-1-3.cpp
@@ -0,0 +1,39 @@
+#include "ParkingLot.h"
+#include "Car.h"
+#include "Bus.h"
+#include "Motorbike.h"
+
+int main(){
+    ParkingLot parking(10);
+    string typeOfVehicelParked = "";
+    Vehicle* vehicle;
+    int count = 0;
+
+    while(true){
+        cout<<"Enter the type of vehicle they want to park:- "<<endl;
+        cin>>typeOfVehicelParked;
+
+        if(typeOfVehicelParked == "car"){
+            count++;
+            vehicle = new Car(count);
+            parking.parkVehicle(vehicle);
+        }
+        if(typeOfVehicelParked == "bus"){
+            count++;
+            vehicle = new Bus(count);
+            parking.parkVehicle(vehicle);
+        }
+        if(typeOfVehicelParked == "motorbike"){
+            count++;
+            vehicle = new Motorbike(count);
+            parking.parkVehicle(vehicle);
+        }
+
+        if(count == 11){
+            break;
+        }
+    }
+
+    cout<<"Number of vehicles overstayed = "<<parking.countOverStayingVehicles(15)<<endl;
+    return 0;
+}
