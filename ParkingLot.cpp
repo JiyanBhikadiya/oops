380ceef0541f925917afacabdddda451862e9ac6
Jiyan2901 <you@example.com>
2024-08-29 22:18:27 +0930

    main

--- ParkingLot.cpp
+++ ParkingLot.cpp
@@ -1,14 +1,54 @@
+// #include "ParkingLot.h"
+
+// ParkingLot::ParkingLot() {
+//     currentCapacity = 0;
+//     maxNumOfVehicles = 0;
+//     vehicles = new Vehicle*[maxNumOfVehicles];
+// }
+
+// ParkingLot::ParkingLot(int maxNumOfVehicles) {
+//     currentCapacity = 0;
+//     this->maxNumOfVehicles = maxNumOfVehicles;
+//     vehicles = new Vehicle*[maxNumOfVehicles];
+// }
+
+// int ParkingLot::getCount() {
+//     return currentCapacity;
+// }
+
+// void ParkingLot::parkVehicle(Vehicle* vehicleWaiting) {
+//     if(currentCapacity < maxNumOfVehicles){
+//         vehicles[currentCapacity] = vehicleWaiting;
+//         currentCapacity++;
+
+//         // cout<<"Vehicle "<<vehicleWaiting->getID()<<" Parked"<<endl;
+//     }
+//     else{
+//         cout<<"The lot is full"<<endl;
+//     }
+// }
+
+// void ParkingLot::unparkVehicle(int ID) {
+//     bool isVehiclePresent = false;
+//     for(int i=0;i<currentCapacity;i++){
+//         if(ID == vehicles[i]->getID()){
+//             // cout<<"Vehicle "<<ID<<" Unparked"<<endl;
+//             isVehiclePresent = true;
+//             currentCapacity--;
+//             for(int j = i;j<currentCapacity-1;j++){
+//                 vehicles[j] = vehicles[j+1];
+//             }
+//         }
+//     }
+
+//     if(!isVehiclePresent){
+//         cout<<"Vehicle not in the lot"<<endl;
+//     }
+// }
 #include "ParkingLot.h"
+#include <iostream>
 
-ParkingLot::ParkingLot() {
-    currentCapacity = 0;
-    maxNumOfVehicles = 0;
-    vehicles = new Vehicle*[maxNumOfVehicles];
-}
-
-ParkingLot::ParkingLot(int maxNumOfVehicles) {
-    currentCapacity = 0;
-    this->maxNumOfVehicles = maxNumOfVehicles;
+ParkingLot::ParkingLot(int capacity) : maxNumOfVehicles(capacity), currentCapacity(0) {
     vehicles = new Vehicle*[maxNumOfVehicles];
 }
 
@@ -16,32 +56,29 @@ int ParkingLot::getCount() {
     return currentCapacity;
 }
 
-void ParkingLot::parkVehicle(Vehicle* vehicleWaiting) {
-    if(currentCapacity < maxNumOfVehicles){
-        vehicles[currentCapacity] = vehicleWaiting;
-        currentCapacity++;
-
-        // cout<<"Vehicle "<<vehicleWaiting->getID()<<" Parked"<<endl;
-    }
-    else{
-        cout<<"The lot is full"<<endl;
+void ParkingLot::parkVehicle(Vehicle* vehicle) {
+    if (currentCapacity < maxNumOfVehicles) {
+        vehicles[currentCapacity++] = vehicle;
+    } else {
+        std::cout << "The lot is full" << std::endl;
     }
 }
 
-void ParkingLot::unparkVehicle(int ID) {
-    bool isVehiclePresent = false;
-    for(int i=0;i<currentCapacity;i++){
-        if(ID == vehicles[i]->getID()){
-            // cout<<"Vehicle "<<ID<<" Unparked"<<endl;
-            isVehiclePresent = true;
-            currentCapacity--;
-            for(int j = i;j<currentCapacity-1;j++){
-                vehicles[j] = vehicles[j+1];
+void ParkingLot::unparkVehicle(int id) {
+    bool found = false;
+    for (int i = 0; i < currentCapacity; ++i) {
+        if (vehicles[i]->getID() == id) {
+            delete vehicles[i];
+            for (int j = i; j < currentCapacity - 1; ++j) {
+                vehicles[j] = vehicles[j + 1];
             }
+            found = true;
+            break;
         }
     }
-
-    if(!isVehiclePresent){
-        cout<<"Vehicle not in the lot"<<endl;
+    if (!found) {
+        std::cout << "Vehicle not in the lot" << std::endl;
+    } else {
+        currentCapacity--;
     }
 }
