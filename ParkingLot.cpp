1318e8dfd48dedc31445a71b488424ab73a967d1
Jiyan2901 <you@example.com>
2024-08-29 21:39:20 +0930

    main

--- ParkingLot.cpp
+++ ParkingLot.cpp
@@ -35,15 +35,18 @@ void ParkingLot::unparkVehicle(int ID) {
     for(int i=0;i<currentCapacity;i++){
         if(ID == vehicles[i].getID()){
             // cout<<"Vehicle "<<ID<<" Unparked"<<endl;
-            isVehiclePresent = 1;
+            isVehiclePresent = 0;
             currentCapacity--;
             for(int j = i;j<currentCapacity;j++){
                 vehicles[i] = vehicles[j];
             }
         }
+        else{
+            isVehiclePresent = 1;
+        }
     }
 
-    if(isVehiclePresent == 0){
+    if(isVehiclePresent == 1){
         cout<<"Vehicle not in the lot"<<endl;
     }
 }
