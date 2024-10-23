2b81c5621d0c58439dab1efb4696d7d935cd18c4
Jiyan2901 <you@example.com>
2024-10-24 09:21:29 +1030

    hpp

---
+++ Robot.hpp
@@ -0,0 +1,22 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+class Robot
+{
+private:
+    /* data */
+public:
+    Robot(/* args */);
+    ~Robot();
+};
+
+Robot::Robot(/* args */)
+{
+}
+
+Robot::~Robot()
+{
+}
+
+
+#endif
