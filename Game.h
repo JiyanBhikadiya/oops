cd901abaa0884bddbf8d4016d4eaa72d11b326b0
Jiyan2901 <you@example.com>
2024-10-23 21:21:34 +1030

    changes

--- Game.h
+++ Game.h
@@ -6,31 +6,32 @@
 #include "Mine.h"
 #include "Utils.h"
 #include <vector>
-using namespace std;
 
-class Game
-{
-private:
-    vector<GameEntity*> entities;
-public:
-    Game(/* args */);
-    std::vector<GameEntity*> initGame(int numShips, int numMines, int gridWidth, int gridHeight);
-    void gameLoop(int maxIterations, double mineDistanceThreshold);
-    ~Game();
+
+class Game {
+    private:
+        vector<GameEntity*> entities;
+    
+    public:
+        vector<GameEntity*> initGame(int numShips, int numMines, int gridWidth, int gridHeight);
+        void gameLoop(int maxIterations, double mineDistanceThreshold);
+
+        vector<GameEntity*> get_entities();
+        void set_entities(vector<GameEntity*> entities);
+
+
 };
 
-Game::Game(/* args */)
-{
-}
 
-std::vector<GameEntity*> Game::initGame(int numShips, int numMines,int gridWidth, int gridHeight) {
-  Utils u;
-
-  for (int i = 0; i< numShips; i++) {
+vector<GameEntity*> Game:: initGame(int numShips, int numMines, int gridWidth, int gridHeight) {
+    Utils u;
+    
+    for (int i = 0; i< numShips; i++) {
         tuple<int, int> randomPos = u.generateRandomPos(gridWidth,gridHeight);
         Ship* ship = new Ship(get<0>(randomPos),get<1>(randomPos));
         entities.push_back(ship);
     }
+
     for (int i = 0; i< numMines; i++) {
         tuple<int, int> randomPos = u.generateRandomPos(gridWidth,gridHeight);
         Mine* ship = new Mine(get<0>(randomPos),get<1>(randomPos));
@@ -38,38 +39,56 @@ std::vector<GameEntity*> Game::initGame(int numShips, int numMines,int gridWidth
     }
 
     return entities;
-}
 
-void Game:: gameLoop(int maxIterations, double mineDistanceThreshold) {
-    Utils u;
-
-    for (int i = 0; i < maxIterations; i++) {
-
-        for (int j = 0; j < entities.size(); j++){
-            GameEntity* entity1 = entities[j];
-            if (entity1->getType()== ShipType) {
-                static_cast<Ship*>(entity1)->move(1,0);
-
-                for (int k = 0; k < entities.size(); k++) { 
-                    GameEntity* entity2 = entities[k];
-                    if (entity2->getType()== MineType) {
-                        if ((u.calculateDistance(entity1->getPos(),entity2->getPos()) <= mineDistanceThreshold)) {
-                            static_cast<Mine*>(entity2)->explode();
-
-                            tuple<int, int> position = entity1->getPos();
-                            Explosion explosion(get<0>(position),get<1>(position));
-                            explosion.apply(*entity1);
-                        }
-                    }
-                }
-            }
-        }
-    }
 };
 
-Game::~Game()
-{
-}
+
+    void Game:: gameLoop(int maxIterations, double mineDistanceThreshold) {
+        Utils u;
+
+        for (int i = 0; i < maxIterations; i++) {
+
+            for (int j = 0; j < entities.size(); j++){
+                GameEntity* entity1 = entities[j];
+                if (entity1->getType()== ShipType) {
+                    static_cast<Ship*>(entity1)->move(1,0);
+
+                    // if (u.calculateDistance())
+
+                    for (int k = 0; k < entities.size(); k++) { 
+                        GameEntity* entity2 = entities[k];
+                        if (entity2->getType()== MineType) {
+                            if ((u.calculateDistance(entity1->getPos(),entity2->getPos()) <= mineDistanceThreshold)) {
+                                static_cast<Mine*>(entity2)->explode();
+
+                                tuple<int, int> position = entity1->getPos();
+                                Explosion explosion(get<0>(position),get<1>(position));
+                                explosion.apply(*entity1);
+                            }
+                        }
+                    }
+
+
+                }
+
+            }
+        }
+    };
+
+
+
+
+
+vector<GameEntity*> Game:: get_entities() {
+    return entities;
+};
+
+
+
+void Game:: set_entities(vector<GameEntity*> entities) {
+    this->entities = entities;
+};
+
 
 
 #endif
