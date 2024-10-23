48cdd8f57c98c579c693f3d18b84977bc00c428e
Jiyan2901 <you@example.com>
2024-10-23 19:53:27 +1030

    changes

--- Utils.h
+++ Utils.h
@@ -18,8 +18,8 @@ class Utils : public Effect{
         };
         static double calculateDistance(std::tuple<int, int> pos1, std::tuple<int, int> pos2){
             int x1 = get<0>(pos1);
-            int x2 = get<1>(pos1);
-            int y1 = get<0>(pos2);
+            int y1 = get<1>(pos1);
+            int x2 = get<0>(pos2);
             int y2 = get<1>(pos2);
 
             return std::sqrt(std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2));
