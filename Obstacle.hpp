c41ce1a1d0b571bb8d2a7482ae898897f831508c
Jiyan2901 <you@example.com>
2024-10-24 10:26:00 +1030

    change

--- Obstacle.hpp
+++ Obstacle.hpp
@@ -1,22 +1,36 @@
 #ifndef OBSTACLE_H
 #define OBSTACLE_H
 
-class Obstacle
+#include "Interactable.hpp"
+#include "Robot.hpp"
+#include <cmath>
+
+class Obstacle : public Interactable
 {
 private:
+int x;
+    int y;
+    int width;
+    int height;
     /* data */
 public:
-    Obstacle(/* args */);
-    ~Obstacle();
+    Obstacle(int x, int y, int width, int height):Interactable(x,y,1,1){}
+
+    bool interact(Robot* robot) override {
+        // Calculate the Euclidean distance between the robot and the obstacle
+        double robotDistance = std::sqrt(std::pow((*robot).getCoordinates().first - x, 2) + std::pow((*robot).getCoordinates().second - y, 2));
+        
+        if (robotDistance == 0) {
+            (*robot).takeHit(); 
+            return (*robot).getHealth() <= 0;
+        }
+        return false;
+    }
+
+    InteractableType getType() override {
+        return OBSTACLE;
+    }
 };
-
-Obstacle::Obstacle(/* args */)
-{
-}
-
-Obstacle::~Obstacle()
-{
-}
 
 
 #endif
