a5ad9056c0c45714c492e337105feda4a37672df
Jiyan2901 <you@example.com>
2024-10-23 19:29:14 +1030

    cpp delete

--- GameEntity.h
+++ GameEntity.h
@@ -25,4 +25,24 @@ class GameEntity
         GameEntityType getType();
 };
 
+
+GameEntity::GameEntity() {
+    make_tuple(0,0);
+}
+
+GameEntity::GameEntity(int x, int y, char type) {
+    cout<<"Hello"<<endl;
+    make_tuple(x,y);
+    type = type;
+}
+
+std::tuple<int, int> GameEntity::getPos() {
+    return position;
+}
+
+GameEntityType GameEntity::getType() {
+    return type;
+}
+
+
 #endif
